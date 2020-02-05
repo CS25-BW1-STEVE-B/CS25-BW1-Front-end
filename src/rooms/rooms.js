8fd132350d7778dbf57a9f28b52474e532fbe70b
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-02-05 17:19:10 -0500

    latest updates

--- src/rooms/rooms.js
+++ src/rooms/rooms.js
@@ -3,7 +3,7 @@ import './rooms.scss'
 
 const Rooms = props => {
     console.log('from rooms', props.rooms)
-    const rooms = []
+    let rooms = []
     if(props.rooms !== undefined){
         props.rooms.forEach(room => {
             rooms.push(room)
@@ -19,34 +19,38 @@ const Rooms = props => {
         // variable that holds our primary array
         let sub_arr = []
         // for loop used to come up with numbers for position x in our sub arrays
-        for(let i = 0; i < 5; i++){
+        for(let i = 0; i <= 11; i++){
             // for loop used to come up with numbers for position y in our sub arrays
-            for(let j = 0; j < 5; j++){
+            for(let j = 0; j <= 11; j++){
                 // we push our sub array created to our primary array var
                 sub_arr.push({x: i, y: j})
             }
         }
         // we assign our dummy variable to be our primary array
-        return test_rooms = sub_arr
+        return rooms = sub_arr
     }
     the2DArray()
 
-    console.log('2D ARRAY', test_rooms)
+    console.log('2D ARRAY', rooms)
+    
     
     return (
         <>
-            {test_rooms.map(room => {
-                return <div className='testRooms' id={room}></div>
+            {props.rooms !== undefined ? 
+                props.rooms.map(room => {
+                    return <div className='rooms' id={room.pk}>{room.fields.title}</div>
+                })
+            : null}
+            {rooms.map(room => {
+                // console.log('GRID', room)
+                return <div className='invisibleRooms'></div>
+                // return(
+                //     <div id={`${room.pk}`} style={{border: '1px solid black', width: '3rem', height: '3rem'}}>
+                //         <h2 className='roomName'>{room.fields.title}</h2>
+                //         {/* <p>{room.fields.description}</p> */}
+                //     </div>
+                // )
             })}
-
-            {/* {rooms.map(room => {
-                return(
-                    <div id={`${room.pk}`} style={{border: '1px solid black', width: '3rem', height: '3rem'}}>
-                        <h2 className='roomName'>{room.fields.title}</h2>
-                        {/* <p>{room.fields.description}</p>*/}
-                    {/* </div> */}
-                {/* ) */}
-            {/* })} */}
             
         </>
     )
