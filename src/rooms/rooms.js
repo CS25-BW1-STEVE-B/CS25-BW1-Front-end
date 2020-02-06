b88c8bbb21d8268554fe18c615a3aa74bd89d714
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-02-06 16:23:17 -0500

    making great progress now

--- src/rooms/rooms.js
+++ src/rooms/rooms.js
@@ -30,6 +30,45 @@ const Rooms = props => {
                 x: '',
                 y: '',
             }
+        },
+        {
+            title: "Grand Overlook",
+            description: "A steep cliff appears before you, falling↵into the darkness. Ahead to the north, a light flickers in↵the distance, but there is no way across the chasm.",
+            pk: 3,
+            n_to: 0,
+            s_to: 2,
+            e_to: 0,
+            w_to: 0,
+            coords: {
+                x: '',
+                y: '',
+            }
+        },
+        {
+            title: "Narrow Passage",
+            description: "The narrow passage bends here from west↵to north. The smell of gold permeates the air.",
+            pk: 4,
+            n_to: 5,
+            s_to: 0,
+            e_to: 0,
+            w_to: 2,
+            coords: {
+                x: '',
+                y: '',
+            }
+        },
+        {
+            title: "Treasure Chamber",
+            description: "You've found the long-lost treasure↵chamber! Sadly, it has already been completely emptied by↵earlier adventurers. The only exit is to the south.",
+            pk: 5,
+            n_to: 0,
+            s_to: 4,
+            e_to: 0,
+            w_to: 0,
+            coords: {
+                x: '',
+                y: '',
+            }
         }
     ]
 
@@ -72,7 +111,7 @@ const Rooms = props => {
 
         for(let i = 0; i < array2d.length; i++){
             if(array2d[i].x === first_room.coords.x && array2d[i].y === first_room.coords.y){
-                console.log('rooms[0]', rooms[0])
+                // console.log('rooms[0]', rooms[0])
                 array2d[i] = rooms[i]
             // let room = <div className='visibleRooms' x={first_room_coords.x} y={first_room_coords.y}>{rooms[0].title}</div>
                 new_rooms.push(first_room)
@@ -80,13 +119,33 @@ const Rooms = props => {
                     for(let j = 0; j < rooms.length; j++){
                         // the room in the current index north to is equal to any primary key of the other rooms
                         if(rooms[z].n_to === rooms[j].pk){
-                            rooms[j].coords.x = rooms[z].coords.x - 1
-                            rooms[j].coords.y = rooms[z].coords.y
-                            let next_room = rooms[j]
-                            console.log('WHATS NEXT ROOM', next_room)
+                            rooms[j].coords.x = rooms[z].coords.x
+                            rooms[j].coords.y = rooms[z].coords.y - 1
                             // array2d[i] = rooms[j]
                             // // let new_room = <div className='visibleRooms' x={rooms[j].coords.x} y={rooms[j].coords.y}>{rooms[0].title}</div>
-                            new_rooms.push(next_room)
+                            new_rooms.push(rooms[j])
+                        } 
+                        // the room in the current index north to is equal to any primary key of the other rooms
+                        else if(rooms[z].w_to === rooms[j].pk){
+                            rooms[j].coords.x = rooms[z].coords.x - 1
+                            rooms[j].coords.y = rooms[z].coords.y
+                            // array2d[i] = rooms[j]
+                            // // let new_room = <div className='visibleRooms' x={rooms[j].coords.x} y={rooms[j].coords.y}>{rooms[0].title}</div>
+                            new_rooms.push(rooms[j])
+                        } 
+                        else if(rooms[z].s_to === rooms[j].pk){
+                            rooms[j].coords.x = rooms[z].coords.x
+                            rooms[j].coords.y = rooms[z].coords.y + 1
+                            // array2d[i] = rooms[j]
+                            // // let new_room = <div className='visibleRooms' x={rooms[j].coords.x} y={rooms[j].coords.y}>{rooms[0].title}</div>
+                            new_rooms.push(rooms[j])
+                        } 
+                        else if(rooms[z].e_to === rooms[j].pk){
+                            rooms[j].coords.x = rooms[z].coords.x + 1
+                            rooms[j].coords.y = rooms[z].coords.y
+                            // array2d[i] = rooms[j]
+                            // // let new_room = <div className='visibleRooms' x={rooms[j].coords.x} y={rooms[j].coords.y}>{rooms[0].title}</div>
+                            new_rooms.push(rooms[j])
                         } 
                     }
                 }
@@ -104,7 +163,8 @@ const Rooms = props => {
     }
 
     console.log('2D ARRAY', array2DRooms)
-    console.log('API ROOMS', rooms)
+    console.log('TEST ROOMS', rooms)
+    console.log('API ROOMS', props.rooms)
     
     return (
         <>
@@ -115,7 +175,7 @@ const Rooms = props => {
                 })
             : null} */}
             {/* {array2DRooms.map(room => {
-                // console.log('GRID', room)
+                console.log('GRID', room)
                 return <div className='invisibleRooms' x={room.x} y={room.y}></div>
             })} */}
             
